7ff10910d48ae20e1c4e3774a46cf972dc88a92f
Frostbyte178 <frostbyte178@gmail.com>
2024-07-12 15:27:15 -0700

    parity

--- dreadv1.js
+++ dreadv1.js
@@ -1,5 +1,5 @@
 const { combineStats, makeAuto, weaponArray, makeTurret } = require('../facilitators.js');
-const { smshskl, base } = require('../constants.js');
+const { smshskl, base, basePolygonDamage, basePolygonHealth } = require('../constants.js');
 const g = require('../gunvals.js');
 const dreadnoughtBody = {
     SPEED: base.SPEED * 0.5,
@@ -47,7 +47,6 @@ g.dreadv1Trap = {
 	damage: 1.2,
 	health: 1.15,
 	resist: 1.1,
-	recoil: 0,
 	size: 1.3,
 }
 
@@ -56,6 +55,198 @@ return console.log('--- Dreadnoughts v1 addon [dreadv1.js] is disabled. See line
 
 // Set the below variable to true to enable the Medicare and Medicaid healing bodies.
 const enableHealers = true;
+
+// Food
+Class.hexagonOfficialV1 = {
+	PARENT: 'food',
+	COLOR: 'magenta',
+	LABEL: "Hexagon",
+	BODY: {
+        DAMAGE: 2 * basePolygonDamage,
+        DENSITY: 80,
+        HEALTH: 600 * basePolygonHealth,
+        RESIST: Math.pow(1.25, 3),
+        PENETRATION: 1.1,
+        SHIELD: 40 * basePolygonHealth,
+        ACCELERATION: 0.0025
+    },
+	VALUE: 21000,
+	SHAPE: 6,
+	SIZE: 70,
+	DRAW_HEALTH: true,
+    GIVE_KILL_MESSAGE: true,
+}
+Class.heptagonOfficialV1 = {
+	PARENT: 'food',
+	COLOR: 'green',
+	LABEL: "Heptagon",
+	BODY: {
+        DAMAGE: 2 * basePolygonDamage,
+        DENSITY: 80,
+        HEALTH: 750 * basePolygonHealth,
+        RESIST: Math.pow(1.25, 3),
+        PENETRATION: 1.1,
+        SHIELD: 50 * basePolygonHealth,
+        ACCELERATION: 0.0025
+    },
+	VALUE: 28000,
+	SHAPE: 7,
+	SIZE: 80,
+	DRAW_HEALTH: true,
+    GIVE_KILL_MESSAGE: true,
+}
+Class.octagonOfficialV1 = {
+	PARENT: 'food',
+	COLOR: 'hexagon',
+	LABEL: "Octagon",
+	BODY: {
+        DAMAGE: 2 * basePolygonDamage,
+        DENSITY: 80,
+        HEALTH: 900 * basePolygonHealth,
+        RESIST: Math.pow(1.25, 3),
+        PENETRATION: 1.1,
+        SHIELD: 60 * basePolygonHealth,
+        ACCELERATION: 0.0025
+    },
+	VALUE: 35000,
+	SHAPE: 8,
+	SIZE: 90,
+	DRAW_HEALTH: true,
+    GIVE_KILL_MESSAGE: true,
+}
+Class.nonagonOfficialV1 = {
+	PARENT: 'food',
+	COLOR: 'white',
+	LABEL: "Nonagon",
+	BODY: {
+        DAMAGE: 2 * basePolygonDamage,
+        DENSITY: 80,
+        HEALTH: 1050 * basePolygonHealth,
+        RESIST: Math.pow(1.25, 3),
+        PENETRATION: 1.1,
+        SHIELD: 70 * basePolygonHealth,
+        ACCELERATION: 0.0025
+    },
+	VALUE: 42000,
+	SHAPE: 9,
+	SIZE: 100,
+	DRAW_HEALTH: true,
+    GIVE_KILL_MESSAGE: true,
+}
+
+// Map elements
+function portalRings(color = '#1c3766') {
+	return [
+		{
+			POSITION: [22, 0, 0, 0, 1],
+			TYPE: ['portalRing1OfficialV1', {COLOR: color}]
+		}, {
+			POSITION: [20.5, 0, 0, -45, 1],
+			TYPE: ['portalRing2OfficialV1', {COLOR: color}]
+		}, {
+			POSITION: [20.5, 0, 0, 135, 1],
+			TYPE: ['portalRing3OfficialV1', {COLOR: color}]
+		}, {
+			POSITION: [21.2, 0, 0, 0, 1],
+			TYPE: ['portalRing4OfficialV1', {COLOR: color}]
+		}
+	]
+}
+Class.portalRing1OfficialV1 = {
+	SHAPE: "M -1 0 A 1 1 90 0 0 1 0 L 0.7 0 A 0.7 0.7 90 0 1 -0.7 0 Z M -1 0 A 1 1 90 0 1 1 0 L 0.7 0 A 0.7 0.7 90 0 0 -0.7 0 Z",
+	COLOR: "#1c3766",
+	BORDERLESS: true,
+}
+Class.portalRing2OfficialV1 = {
+	SHAPE: "M -0.707 0.707 A 1 1 0 0 1 -0.707 -0.707 A 1 1.225 0 0 0 -0.707 0.707 Z",
+	COLOR: {BASE: "#1c3766", BRIGHTNESS_SHIFT: 16, SATURATION_SHIFT: 0.7},
+	BORDERLESS: true,
+}
+Class.portalRing3OfficialV1 = {
+	SHAPE: "M -0.5 0.866 A 1 1 0 0 1 -0.5 -0.866 A 1 1.1 0 0 0 -0.5 0.866 Z",
+	COLOR: {BASE: "#1c3766", BRIGHTNESS_SHIFT: -6, SATURATION_SHIFT: 1.1},
+	BORDERLESS: true,
+}
+Class.portalRing4OfficialV1 = {
+	SHAPE: "M -0.92 0 A 0.92 0.92 90 0 0 0.92 0 L 0.84 0 A 0.84 0.84 90 0 1 -0.84 0 Z M -0.92 0 A 0.92 0.92 90 0 1 0.92 0 L 0.84 0 A 0.84 0.84 90 0 0 -0.84 0 Z",
+	COLOR: {BASE: "#1c3766", BRIGHTNESS_SHIFT: 7, SATURATION_SHIFT: 0.9},
+	BORDERLESS: true,
+}
+Class.portalOfficialV1 = {
+	LABEL: "",
+	TYPE: 'portal',
+	BODY: {
+		HEALTH: 1e10,
+		SHIELD: 1e10,
+		REGEN: 1e10,
+		DAMAGE: 0,
+		PENETRATION: 1e10,
+		DENSITY: 1e10,
+		RANGE: 2000,
+	},
+	ALWAYS_ACTIVE: true,
+	HITS_OWN_TYPE: 'never',
+	GIVE_KILL_MESSAGE: false,
+	DRAW_HEALTH: false,
+	TEAM: TEAM_ROOM,
+	COLOR: 'pureBlack',
+	FACING_TYPE: 'noFacing',
+	SIZE: 33,
+}
+Class.spikyPortalSpikesOfficialV1 = {
+	SHAPE: "",
+	INDEPENDENT: true,
+	FACING_TYPE: ["spin", {speed: 0.07}],
+	GUNS: weaponArray({
+		POSITION: [30, 8, 0.001, 0, 0, 0, 0],
+		PROPERTIES: {COLOR: 'egg'}
+	}, 5),
+}
+Class.spikyPortalBumpsOfficialV1 = {
+	SHAPE: "M 1 0 L 0.666 0.216 L 0.566 0.41 L 0.41 0.566 L 0.309 0.951 L 0 0.7 L -0.215 0.665 L -0.412 0.565 L -0.809 0.588 L -0.666 0.217 L -0.699 0.001 L -0.664 -0.217 L -0.809 -0.588 L -0.412 -0.566 L -0.217 -0.664 L 0.001 -0.699 L 0.309 -0.951 L 0.411 -0.567 L 0.565 -0.412 L 0.665 -0.215 Z",
+	COLOR: 'egg',
+	INDEPENDENT: true,
+	FACING_TYPE: ["spin", {speed: 0.12}],
+}
+Class.spikyPortalOfficialV1 = {
+	PARENT: 'portalOfficialV1',
+	TURRETS: [
+		{
+			POSITION: [26, 0, 0, 0, 0, 0],
+			TYPE: "spikyPortalSpikesOfficialV1"
+		}, {
+			POSITION: [35, 0, 0, 0, 0, 0],
+			TYPE: "spikyPortalBumpsOfficialV1"
+		}
+	],
+	PROPS: [
+		{
+			POSITION: [20, 0, 0, 0, 1],
+			TYPE: ['egg', {COLOR: '#212121'}]
+		},
+		...portalRings('#1c1c1c')
+	]
+}
+Class.bluePortalOfficialV1 = {
+	PARENT: 'portalOfficialV1',
+	PROPS: [
+		{
+			POSITION: [20, 0, 0, 0, 1],
+			TYPE: ['egg', {COLOR: 'black'}]
+		},
+		...portalRings()
+	]
+}
+Class.greenPortalOfficialV1 = {
+	PARENT: 'portalOfficialV1',
+	PROPS: [
+		{
+			POSITION: [20, 0, 0, 0, 1],
+			TYPE: ['egg', {COLOR: 'black'}]
+		},
+		...portalRings('#1c6620')
+	]
+}
 
 // Misc
 Class.genericDreadnought1 = {
@@ -76,7 +267,7 @@ Class.dreadv1BodyTurret = makeTurret({
 	GUNS: [{
 		POSITION: [22, 10, 1, 0, 0, 0, 0],
 		PROPERTIES: {
-			SHOOT_SETTINGS: combineStats([g.basic, g.pelleter, g.power, { recoil: 1.4 }, g.turret, { health: 0.75, speed: 0.4, maxSpeed: 0.4, reload: 0.7 }]),
+			SHOOT_SETTINGS: combineStats([g.basic, g.pelleter, g.power, { recoil: 0.7 }, g.turret, { size: 0.8, health: 0.75, speed: 0.4, maxSpeed: 0.4, reload: 0.7 }]),
 			TYPE: "bullet"
 		}
 	}]
@@ -135,8 +326,7 @@ Class.dreadOfficialV1 = {
 	PARENT: "genericDreadnought1",
 	LABEL: "Dreadnought",
 	UPGRADE_LABEL: "Dreads V1",
-	LEVEL: 150,
-	EXTRA_SKILL: 47,
+	EXTRA_SKILL: 18,
 }
 
 // T1
@@ -145,7 +335,7 @@ Class.swordOfficialV1 = {
 	LABEL: "Sword",
 	UPGRADE_TOOLTIP: "Snipers",
 	GUNS: weaponArray({
-		POSITION: [19, 7, 1, 0, 0, 0, 0],
+		POSITION: [20, 7, 1, 0, 0, 0, 0],
 		PROPERTIES: {
 			SHOOT_SETTINGS: combineStats([g.basic, g.sniper, g.dreadv1Generic, g.dreadv1Sniper]),
 			TYPE: "bullet"
@@ -158,7 +348,7 @@ Class.pacifierOfficialV1 = {
 	LABEL: "Pacifier",
 	UPGRADE_TOOLTIP: "Bullet Spam",
 	GUNS: weaponArray({
-		POSITION: [15, 7, 1, 0, 0, 0, 0],
+		POSITION: [15.5, 7.25, 1, 0, 0, 0, 0],
 		PROPERTIES: {
 			SHOOT_SETTINGS: combineStats([g.basic, g.dreadv1Generic, g.dreadv1Slow]),
 			TYPE: "bullet"
@@ -190,9 +380,9 @@ Class.centaurOfficialV1 = {
 	UPGRADE_TOOLTIP: "Traps",
 	GUNS: weaponArray([
 		{
-			POSITION: [12.5, 7, 1, 0, 0, 0, 0],
+			POSITION: [13, 7, 1, 0, 0, 0, 0],
 		}, {
-			POSITION: [2.5, 7, 1.6, 12.5, 0, 0, 0],
+			POSITION: [3, 7, 1.5, 13, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.trap, g.dreadv1Generic, g.dreadv1Slow, g.dreadv1Trap]),
 				TYPE: ["trap", {HITS_OWN_TYPE: "never"} ],
@@ -250,13 +440,13 @@ Class.sabreOfficialV1 = {
 	UPGRADE_TOOLTIP: "Assassins",
 	GUNS: weaponArray([
 		{
-			POSITION: [26, 7, 1, 0, 0, 0, 0],
+			POSITION: [27, 7, 1, 0, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.basic, g.sniper, g.assassin, g.dreadv1Generic, g.dreadv1Sniper]),
 				TYPE: "bullet"
 			}
 		}, {
-			POSITION: [4, 7, -1.4, 9, 0, 0, 0]
+			POSITION: [3.5, 7, -1.4, 9, 0, 0, 0]
 		}
 	], 3)
 }
@@ -266,9 +456,9 @@ Class.gladiusOfficialV1 = {
 	UPGRADE_TOOLTIP: "Rifles",
 	GUNS: weaponArray([
 		{
-			POSITION: [20, 9, 1, 0, 0, 0, 0]
+			POSITION: [17, 9, 1, 0, 0, 0, 0]
 		}, {
-			POSITION: [23, 6, 1, 0, 0, 0, 0],
+			POSITION: [20, 6, 1, 0, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.basic, g.sniper, g.rifle, g.dreadv1Generic, g.dreadv1Sniper, {damage: 1.05}]),
 				TYPE: "bullet"
@@ -283,15 +473,15 @@ Class.appeaserOfficialV1 = {
 	UPGRADE_TOOLTIP: "Machine Guns",
 	GUNS: weaponArray([
 		{
-			POSITION: [6, 8, 1.3, 7, 0, 0, 0],
+			POSITION: [8.5, 8.875, 1.25, 7, 0, 0, 0],
 			PROPERTIES: {
-				SHOOT_SETTINGS: combineStats([g.basic, g.machineGun, g.twin, g.dreadv1Generic, g.dreadv1Slow, {health: 1.1, shudder: 1.05, speed: 0.8, maxSpeed: 0.8, range: 0.7, size: 0.75}]),
+				SHOOT_SETTINGS: combineStats([g.basic, g.machineGun, g.twin, g.dreadv1Generic, g.dreadv1Slow, {health: 1.1, shudder: 1.05, speed: 0.85, maxSpeed: 0.8, range: 0.7, size: 0.75}]),
 				TYPE: "bullet"
 			}
 		}, {
-			POSITION: [6, 7.5, 1.2, 9, 0, 0, 0],
+			POSITION: [8.5, 7.875, 1.2, 9, 0, 0, 0],
 			PROPERTIES: {
-				SHOOT_SETTINGS: combineStats([g.basic, g.machineGun, g.twin, g.dreadv1Generic, g.dreadv1Slow, {health: 1.1, shudder: 1.05, speed: 0.8, maxSpeed: 0.8, range: 0.7, size: 0.75 * 8 / 7.5}]),
+				SHOOT_SETTINGS: combineStats([g.basic, g.machineGun, g.twin, g.dreadv1Generic, g.dreadv1Slow, {health: 1.1, shudder: 1.05, speed: 0.85, maxSpeed: 0.8, range: 0.7, size: 0.75 * 8.5 / 7.5}]),
 				TYPE: "bullet"
 			}
 		}
@@ -302,7 +492,7 @@ Class.peacekeeperOfficialV1 = {
 	LABEL: "Peacekeeper",
 	UPGRADE_TOOLTIP: "Heavy Bullets",
 	GUNS: weaponArray({
-		POSITION: [16.5, 10, 1, 0, 0, 0, 0],
+		POSITION: [17.5, 9, 1, 0, 0, 0, 0],
 		PROPERTIES: {
 			SHOOT_SETTINGS: combineStats([g.basic, g.pounder, g.destroyer, g.dreadv1Generic, g.dreadv1Slow, {reload: 1.3, health: 1.3, range: 1.1}]),
 			TYPE: "bullet",
@@ -315,19 +505,19 @@ Class.diplomatOfficialV1 = {
 	UPGRADE_TOOLTIP: "Triplets",
 	GUNS: weaponArray([
 		{
-			POSITION: [14, 4.5, 1, 0, 2.75, 0, 0.5],
+			POSITION: [15.25, 4.75, 1, 0, 3, 0, 0.5],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.basic, g.twin, g.triplet, g.dreadv1Generic, g.dreadv1Slow, {range: 0.9}]),
 				TYPE: "bullet"
 			}
 		}, {
-			POSITION: [14, 4.5, 1, 0, -2.75, 0, 0.5],
+			POSITION: [15.25, 4.75, 1, 0, -3, 0, 0.5],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.basic, g.twin, g.triplet, g.dreadv1Generic, g.dreadv1Slow, {range: 0.9}]),
 				TYPE: "bullet"
 			}
 		}, {
-			POSITION: [15, 4.5, 1, 0, 0, 0, 0],
+			POSITION: [16.25, 4.75, 1, 0, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.basic, g.twin, g.triplet, g.dreadv1Generic, g.dreadv1Slow, {range: 0.9}]),
 				TYPE: "bullet"
@@ -359,19 +549,19 @@ Class.assailantOfficialV1 = {
 	UPGRADE_TOOLTIP: "Minions",
 	GUNS: weaponArray([
 		{
-			POSITION: [13.75, 8, 1, 0, 0, 0, 0],
+			POSITION: [14.25, 9, 1, 0, 0, 0, 0],
 		}, {
-			POSITION: [1, 10, 1, 13.75, 0, 0, 0],
+			POSITION: [1.5, 10, 1, 14.25, 0, 0, 0],
 			PROPERTIES: {
 				MAX_CHILDREN: 4,
-				SHOOT_SETTINGS: combineStats([g.factory, g.overseer, g.dreadv1Drone, {damage: 0.6, speed: 0.85, maxSpeed: 0.85}]),
-				TYPE: ["minion", {GUN_STAT_SCALE: {health: 0.45, speed: 0.8, maxSpeed: 0.8}}],
+				SHOOT_SETTINGS: combineStats([g.factory, g.overseer, g.dreadv1Drone, {damage: 0.6}]),
+				TYPE: ["minion", {GUN_STAT_SCALE: {reload: 1.333, health: 0.7, speed: 0.8, maxSpeed: 0.8}}],
 				STAT_CALCULATOR: "drone",
 				AUTOFIRE: true,
 				SYNCS_SKILLS: true
 			}
 		}, {
-			POSITION: [12.25, 10, 1, 0, 0, 0, 0]
+			POSITION: [11.5, 10, 1, 0, 0, 0, 0]
 		}
 	], 3)
 }
@@ -411,27 +601,27 @@ Class.cerberusOfficialV1 = {
 	UPGRADE_TOOLTIP: "Trap Spam",
 	GUNS: weaponArray([
 		{
-			POSITION: [13.5, 2.25, 1, 0, 4, 0, 0]
+			POSITION: [13.25, 2.25, 1, 0, 4, 0, 0]
 		}, {
-			POSITION: [1.75, 2.25, 1.7, 13.5, 4, 0, 2/3],
+			POSITION: [1.75, 2.25, 1.7, 13.25, 4, 0, 2/3],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.trap, g.dreadv1Generic, g.dreadv1Slow, g.dreadv1Trap, { reload: 1.5, health: 0.75, damage: 0.8 }]),
 				TYPE: ["trap", {HITS_OWN_TYPE: "never"} ],
 				STAT_CALCULATOR: "trap",
 			},
 		}, {
-			POSITION: [13.5, 2.25, 1, 0, -4, 0, 0]
+			POSITION: [13.25, 2.25, 1, 0, -4, 0, 0]
 		}, {
-			POSITION: [1.75, 2.25, 1.7, 13.5, -4, 0, 1/3],
+			POSITION: [1.75, 2.25, 1.7, 13.25, -4, 0, 1/3],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.trap, g.dreadv1Generic, g.dreadv1Slow, g.dreadv1Trap, { reload: 1.5, health: 0.75, damage: 0.8 }]),
 				TYPE: ["trap", {HITS_OWN_TYPE: "never"} ],
 				STAT_CALCULATOR: "trap"
 			}
 		}, {
-			POSITION: [15, 3, 1, 0, 0, 0, 0]
+			POSITION: [14.75, 3, 1, 0, 0, 0, 0]
 		}, {
-			POSITION: [2, 3, 1.7, 15, 0, 0, 0],
+			POSITION: [2, 3, 1.7, 14.75, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.trap, g.dreadv1Generic, g.dreadv1Slow, g.dreadv1Trap, { reload: 1.5, health: 0.75, damage: 0.8 }]),
 				TYPE: ["trap", {HITS_OWN_TYPE: "never"} ],
@@ -446,9 +636,9 @@ Class.minotaurOfficialV1 = {
 	UPGRADE_TOOLTIP: "Blocks",
 	GUNS: weaponArray([
 		{
-			POSITION: [13, 9, 1, 0, 0, 0, 0],
+			POSITION: [13, 9.5, 1, 0, 0, 0, 0],
 		}, {
-			POSITION: [3, 9, 1.6, 13, 0, 0, 0],
+			POSITION: [3, 9.5, 1.6, 13, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.trap, g.setTrap, g.dreadv1Generic, g.dreadv1Slow, g.dreadv1Trap, { reload: 1.59, range: 1.05, health: 1.55 }]),
 				TYPE: ["unsetTrap", {HITS_OWN_TYPE: "never"} ],
@@ -462,9 +652,9 @@ Class.sirenOfficialV1 = {
 	LABEL: "Siren",
 	GUNS: weaponArray([
 		{
-			POSITION: [13, 7, -1.5, 0, 0, 0, 0],
+			POSITION: [6, 7, -1.5, 7, 0, 0, 0],
 		}, {
-			POSITION: [2.5, 7, 1.6, 13, 0, 0, 0],
+			POSITION: [3, 7, 1.5, 13, 0, 0, 0],
 			PROPERTIES: {
 				SHOOT_SETTINGS: combineStats([g.trap, g.dreadv1Generic, g.dreadv1Slow, g.dreadv1Trap]),
 				TYPE: ["turretedTrap", {HITS_OWN_TYPE: "never"} ],
@@ -516,7 +706,7 @@ Class.medicaidOfficialV1 = {
 }
 
 Class.addons.UPGRADES_TIER_0.push("dreadOfficialV1");
-	Class.dreadOfficialV1.UPGRADES_TIER_1 = ["swordOfficialV1", "pacifierOfficialV1", "invaderOfficialV1", "centaurOfficialV1"];
+	Class.dreadOfficialV1.UPGRADES_TIER_10 = ["swordOfficialV1", "pacifierOfficialV1", "invaderOfficialV1", "centaurOfficialV1"];
 		Class.swordOfficialV1.UPGRADES_TIER_M1 = ["sabreOfficialV1", "gladiusOfficialV1"];
 		Class.pacifierOfficialV1.UPGRADES_TIER_M1 = ["appeaserOfficialV1", "peacekeeperOfficialV1", "diplomatOfficialV1"];
 		Class.invaderOfficialV1.UPGRADES_TIER_M1 = ["inquisitorOfficialV1", "assailantOfficialV1", "infiltratorOfficialV1"];
@@ -531,10 +721,10 @@ if (!enableHealers) {
 }
 
 // Build both tiers of dreads
-for (let primary of Class.dreadOfficialV1.UPGRADES_TIER_1) {
+for (let primary of Class.dreadOfficialV1.UPGRADES_TIER_10) {
 	let primaryName = primary;
 	primary = ensureIsClass(primary);
-	primary.UPGRADES_TIER_1 = [];
+	primary.UPGRADES_TIER_10 = [];
 
 	for (let secondary of t1Bodies) {
 		let secondaryName = secondary;
@@ -573,10 +763,10 @@ for (let primary of Class.dreadOfficialV1.UPGRADES_TIER_1) {
 		// Actually make that guy
 		Class[definitionName] = {
 			PARENT: "genericDreadnought1",
-			UPGRADES_TIER_2: [],
+			UPGRADES_TIER_12: [],
 			BODY, LABEL, UPGRADE_TOOLTIP, GUNS, TURRETS,
 		};
-		Class[primaryName].UPGRADES_TIER_1.push(definitionName);
+		Class[primaryName].UPGRADES_TIER_10.push(definitionName);
 
 		// Compile T2
 		for (let primary2 of primary.UPGRADES_TIER_M1) {
@@ -622,7 +812,7 @@ for (let primary of Class.dreadOfficialV1.UPGRADES_TIER_1) {
 					PARENT: "genericDreadnought1",
 					BODY, LABEL, UPGRADE_TOOLTIP, GUNS, TURRETS
 				};
-				Class[definitionName].UPGRADES_TIER_2.push(definitionName2);
+				Class[definitionName].UPGRADES_TIER_12.push(definitionName2);
 			}
 		}
 	}
